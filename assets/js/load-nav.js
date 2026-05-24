dfeb4df0348ee7b8d2a28a157d89f679764ffa3a
jsongau <jsongaum@gmail.com>
2026-05-23 22:22:19 -0700

    Update load-nav.js

--- assets/js/load-nav.js
+++ assets/js/load-nav.js
@@ -1,21 +1,5 @@
-async function loadNav() {
-  const navMount = document.querySelector("[data-mega-nav]");
-
-  if (!navMount) return;
-
-  const response = await fetch("/components/mega-nav.html");
-
-  if (!response.ok) {
-    console.error("Could not load mega nav:", response.status);
-    return;
-  }
-
-  navMount.innerHTML = await response.text();
-
-  const script = document.createElement("script");
-  script.src = "/assets/js/mega-nav.js";
-  script.defer = true;
-  document.body.appendChild(script);
-}
-
-loadNav();
+fetch('/components/mega-nav.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('site-nav').innerHTML = html;
+  });
