47308cbeba4df062b19466412658b0f9e0e8bb7a
jsongau <jsongaum@gmail.com>
2026-05-24 20:58:58 -0700

    Update load-nav.js

--- assets/js/load-nav.js
+++ assets/js/load-nav.js
@@ -1,5 +1,16 @@
 fetch('/components/mega-nav.html')
-  .then(res => res.text())
+  .then(res => {
+    if (!res.ok) throw new Error('Mega nav not found at /components/mega-nav.html');
+    return res.text();
+  })
   .then(html => {
-    document.getElementById('site-nav').innerHTML = html;
-  });
+    const target = document.getElementById('site-nav') || document.getElementById('covercapy-mega-nav');
+    if (!target) throw new Error('Missing nav target div. Add <div id="site-nav"></div>');
+    target.innerHTML = html;
+
+    const script = document.createElement('script');
+    script.src = '/assets/js/mega-nav.js';
+    script.defer = true;
+    document.body.appendChild(script);
+  })
+  .catch(err => console.error('CoverCapy mega nav load error:', err));
