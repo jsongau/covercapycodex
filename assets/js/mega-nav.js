520ac33413c3f8e1f3587c67d416749dbc6c9b23
jsongau <jsongaum@gmail.com>
2026-05-24 21:07:12 -0700

    Update mega-nav.js

--- assets/js/mega-nav.js
+++ assets/js/mega-nav.js
@@ -1,12 +1,35 @@
-document.addEventListener('click', function(e){
-  const link = e.target.closest('.cc-link');
-
-  document.querySelectorAll('.cc-link').forEach(item => {
-    if (item !== link) item.classList.remove('is-open');
-  });
-
-  if (link && !e.target.closest('.cc-dropdown')) {
-    e.preventDefault();
-    link.classList.toggle('is-open');
+(function () {
+  function closeAll() {
+    document.querySelectorAll('.cc-link').forEach(el => el.classList.remove('is-open'));
+    document.querySelectorAll('.cc-dropdown').forEach(el => el.classList.remove('is-open'));
   }
-});
+
+  function initMegaNav() {
+    document.querySelectorAll('.cc-link').forEach(link => {
+      const dropdown = link.querySelector('.cc-dropdown') || link.nextElementSibling;
+
+      link.addEventListener('mouseenter', () => {
+        closeAll();
+        link.classList.add('is-open');
+        if (dropdown) dropdown.classList.add('is-open');
+      });
+
+      link.addEventListener('click', e => {
+        e.preventDefault();
+        const open = link.classList.contains('is-open');
+        closeAll();
+        if (!open) {
+          link.classList.add('is-open');
+          if (dropdown) dropdown.classList.add('is-open');
+        }
+      });
+    });
+
+    document.addEventListener('click', e => {
+      if (!e.target.closest('.cc-nav')) closeAll();
+    });
+  }
+
+  window.initMegaNav = initMegaNav;
+  initMegaNav();
+})();
